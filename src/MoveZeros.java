6b48939cffaab79ac4cde9ee53dafbb087cef077
Pavel Grachev <pasha0793@yandex.ru>
2023-04-02 18:36:28 +0400

    02.04.2023

---
+++ src/MoveZeros.java
@@ -0,0 +1,19 @@
+/*
+    Link: https://leetcode.com/problems/move-zeroes
+ */
+public class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int slow = 0;
+        for (int fast = 0; fast < n; fast++) {
+            if (nums[fast] != 0 && nums[slow] == 0) {
+                int tmp = nums[slow];
+                nums[slow] = nums[fast];
+                nums[fast] = tmp;
+            }
+            if (nums[slow] != 0) {
+                slow++;
+            }
+        }
+    }
+}
---
+++ src/TwoSum2.java
@@ -0,0 +1,21 @@
+/*
+    Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted
+ */
+public class TwoSum2 {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                break;
+            }
+            if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return new int[] {++left, ++right};
+    }
+}
