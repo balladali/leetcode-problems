bbb1e2aec464ab09e79c7f0ea2b8690d3dc47240
Pavel Grachev <pasha0793@yandex.ru>
2023-04-13 23:00:39 +0400

    13.04.2023

---
+++ src/ReverseBits.java
@@ -0,0 +1,14 @@
+/*
+    Link: https://leetcode.com/problems/reverse-bits
+ */
+public class ReverseBits {
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            result |= (n & 1);
+            n >>= 1;
+        }
+        return result;
+    }
+}
