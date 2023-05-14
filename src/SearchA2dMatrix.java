3ee1931522d6e8952d61c163b38ca410ce103d60
Pavel Grachev <pasha0793@yandex.ru>
2023-05-14 23:46:55 +0400

    14.05.2023

--- src/SearchA2dMatrix.java
+++ src/SearchA2dMatrix.java
@@ -1,6 +1,6 @@
 /*
     Link: https://leetcode.com/problems/search-a-2d-matrix/
-    Algorithm: Binary Search
+    Algorithm: Binary Search, Two Pointers
  */
 public class SearchA2dMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
@@ -18,6 +18,24 @@ public class SearchA2dMatrix {
                 right = mid - 1;
             } else {
                 left = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    public boolean searchMatrixTwoPointers(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int i = 0;
+        int j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
             }
         }
         return false;
