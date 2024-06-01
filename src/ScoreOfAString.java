1e53fcaf1fe7bcff99c1e099114b9ffe8990dca4
Pavel Grachev <pasha0793@yandex.ru>
2024-06-01 23:45:43 +0400

    01.06.2024

---
+++ src/ScoreOfAString.java
@@ -0,0 +1,12 @@
+/*
+    Link: https://leetcode.com/problems/score-of-a-string
+ */
+public class ScoreOfAString {
+    public int scoreOfString(String s) {
+        int score = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            score += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return score;
+    }
+}
