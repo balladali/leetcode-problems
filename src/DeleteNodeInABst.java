dd37f7b04dabcd63bb5c5672573b6dbbdfcfdb1b
Pavel Grachev <pasha0793@yandex.ru>
2024-10-09 00:15:59 +0400

    08.10.2024

---
+++ src/DeleteNodeInABst.java
@@ -0,0 +1,34 @@
+/*
+    Link: https://leetcode.com/problems/delete-node-in-a-bst
+ */
+public class DeleteNodeInABst {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            root.right = deleteNode(root.right, key);
+        }
+        if (root.val == key) {
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+            TreeNode min = findMin(root.right);
+            root.val = min.val;
+            root.right = deleteNode(root.right, root.val);
+        }
+        return root;
+    }
+
+    private TreeNode findMin(TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+}
